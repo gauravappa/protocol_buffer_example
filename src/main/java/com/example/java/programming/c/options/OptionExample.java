ec0159f8517abd8d8ccca71bf50c2e98aca06b69
Gaurav Appa <gauravappagga@gmail.com>
2022-01-30 16:50:36 +0530

    java option

---
+++ src/main/java/com/example/java/programming/c/options/OptionExample.java
@@ -0,0 +1,13 @@
+package com.example.java.programming.c.options;
+
+
+/* proto file is defined in the
+ * resources com.example.optionproto package
+ */
+public class OptionExample {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
