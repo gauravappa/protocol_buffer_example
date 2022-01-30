82f7be071c1407ef6a0d2001f14aa5c74c969acb
Gaurav Appa <gauravappagga@gmail.com>
2022-01-30 16:30:31 +0530

    first proto programming example

---
+++ src/main/java/com/example/java/programming/a/firstproto/FirstProtoExample.java
@@ -0,0 +1,27 @@
+package com.example.java.programming.a.firstproto;
+
+import com.example.java.programming.a.firstproto.generated.FirstProto.Student;
+
+/* proto file is defined in the
+ * resouces com.example.firstproto package
+ * we defined Student message in proto type
+ * with two field id and name
+ */
+
+public class FirstProtoExample {
+
+	public static void main(String[] args) {
+		
+	/* we are using Student Model from generated classes by protoc compiler from our 
+	 * proto file first_proto.proto
+	 */
+		
+		Student student = Student.newBuilder().setId("15005054")
+							.setName("Gaurav Appa")
+							.build();
+		
+		System.out.println(student.toString());
+		
+	}
+	
+}
